ef525252213098bbf4cc675ea151e6a6471d4aac
mgoblue2023 <douglazovick@gmail.com>
2025-08-13 13:08:07 -0700

    Update App.tsx

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import { Layout } from './components/Layout'
+// app/frontend/src/App.tsx
+import { Layout } from './components/Layout'; // NOTE: capital "L" in Layout
 import { Toaster } from './components/ui/sonner';
 
 export default function App() {
   return (
-    <>
-      <Layout />
+    <Layout>
       <Toaster />
-    </>
+    </Layout>
   );
 }
+
